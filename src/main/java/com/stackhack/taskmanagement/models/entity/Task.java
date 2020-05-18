867aced86f5d0e086c34e0220a37cae71628a623
Yogesh Kumar <yogesh.kumar@nagarro.com>
2020-05-18 19:38:01 +0530

    model mapping and annotation

--- src/main/java/com/stackhack/taskmanagement/models/entity/Task.java
+++ src/main/java/com/stackhack/taskmanagement/models/entity/Task.java
@@ -4,29 +4,71 @@ import java.time.LocalDate;
 import java.util.Date;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+@Entity
 public class Task {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO )
 	private long Id;
 	
-	@Column(name="task_name")
+	@Column(name="task_name",nullable = false,length = 30)
 	private String taskName;
 	
-	@Column(name="task_desc")
-	private String desciption;
+	@Column(name="task_desc",nullable = false,length = 100)
+	private String description;
 	
-	@Column(name="due_date")
+	@Column(name="due_date",nullable = false)
 	private Date dueDate;
 	
 	@CreationTimestamp
 	@Column(name = "creation_date")
 	private Date creationDate;
+
+	public long getId() {
+		return Id;
+	}
+
+	public void setId(long id) {
+		Id = id;
+	}
+
+	public String getTaskName() {
+		return taskName;
+	}
+
+	public void setTaskName(String taskName) {
+		this.taskName = taskName;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Date getDueDate() {
+		return dueDate;
+	}
+
+	public void setDueDate(Date dueDate) {
+		this.dueDate = dueDate;
+	}
+
+	public Date getCreationDate() {
+		return creationDate;
+	}
+
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
 	
 }
